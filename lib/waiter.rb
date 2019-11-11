3fd142b79d8e7e95a72589f8bf94b4fbb8a90661
Wendy Harris <wendyv510@hotmail.com>
2019-11-11 00:30:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,4 +17,14 @@ class Waiter
   def new_meal(customer,total,tip=0)
     Meal.new(self,customer,total,tip)
   end 
+  
+  def waiters 
+    meals.map do |meal|
+      meal.waiters
+    end 
+  end 
+  
+  def meals 
+    Meal.all.select do |meal|
+      
 end
